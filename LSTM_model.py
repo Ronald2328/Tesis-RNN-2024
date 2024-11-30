b54a346d96002b74236cbfb0f0637b45c4238b67
Ronald2328 <ronaldoolivarespalacios@gmail.com>
2024-11-30 09:13:37 -0500

    Dropout

--- LSTM_model.py
+++ LSTM_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class LSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
         super(LSTM, self).__init__()
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
@@ -11,7 +11,8 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size=input_dim, 
                             hidden_size=hidden_dim, 
                             num_layers=layer_dim, 
-                            batch_first=True)  # batch_first para tener (batch, seq, features)
+                            batch_first=True,
+                            dropout=dropout_prob)
         
         # Capa densa de salida
         self.fc = nn.Linear(hidden_dim, output_dim)
